dce1004191167944770bb6a56c74daced93bcad8
Poonam <poonam@codeblock.co.in>
2019-11-12 10:52:46 +0530

    hardcoded function name

--- handlers/addcustomer.js
+++ handlers/addcustomer.js
@@ -95,7 +95,8 @@ class AddCustomer extends BaseHandler {
             //invoke lambda customerresources
 
             let params = {
-                FunctionName: process.env.CREATE_CR_LAMBDA_ARN, //TODO get from process.env
+                //FunctionName: process.env.CREATE_CR_LAMBDA_ARN, //TODO get from process.env
+                FunctionName: 'cw-customers-dev-createcustomerresources',
                 InvocationType: 'Event',
                 Payload: JSON.stringify({ cuid: cuid })
             };
